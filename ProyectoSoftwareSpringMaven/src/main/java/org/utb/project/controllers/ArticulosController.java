5aa02474157a50702045b420d1011c685003ecfe
SnayderMorales <esmider0403@hotmail.com>
2015-09-16 19:46:41 -0500

    Fin de la clase
    Primer paso en javascript

--- ProyectoSoftwareSpringMaven/src/main/java/org/utb/project/controllers/ArticulosController.java
+++ ProyectoSoftwareSpringMaven/src/main/java/org/utb/project/controllers/ArticulosController.java
@@ -5,9 +5,15 @@
  */
 package org.utb.project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.utb.project.dao.ArticuloDao;
+import org.utb.project.entities.Articulo;
+import org.utb.project.entities.Usuario;
 
 /**
  *
@@ -15,8 +21,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class ArticulosController {
+    @Autowired
+    ArticuloDao articuloDao;
+    
+    
     @RequestMapping(value = "/articulos/listado", method = RequestMethod.GET)
     public String listado(){
        return "articulos/listado";
+    }
+    
+     @RequestMapping(path = "/api/articulo", method = RequestMethod.POST)
+     @ResponseBody
+    public void guardar(@ModelAttribute Articulo articulo) {
+        articuloDao.agregar(articulo); 
     }
 }
